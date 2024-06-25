97a39766faac97c4483f33dbe10a66bfd8bbc4b5
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-06-25 22:37:09 +0800

    code doesnt work but im at -912% energy rn so im gonna sleep

--- relation.py
+++ relation.py
@@ -1,24 +1,44 @@
 class Relator:
-    def __init__(self, name, relations=None, children=None):
+    def __init__(self, name, relations=None, children=None, parents=None):
         self.name = name
         self.relations = relations if relations is not None else []
         self.children = children if children is not None else []
-
+        self.parents = parents if parents is not None else []
     def __mul__(self, other):
         self.relations.append(other)
         other.relations.append(self)
     def __add__(self, other):
         self.children.append(other)
+        other.parents.append(self)
     def __str__(self):
         return self.name
     def print_relations(self):
-        print(self.name+"'s Relations:")
         for i in self.relations:
-            print(i.name)
+            print(i)
     def print_children(self):
-        print(self.name+"'s Children:")
         for i in self.children:
-            print(i.name)
+            print(i)
+    def print_parents(self):
+        for i in self.parents:
+            print(i)
+    def relations_in(self,param):
+        included = []
+        for i in param:
+            if i in self.relations:
+                included.append(i)
+        return included
+    def children_in(self,param):
+        included = []
+        for i in param:
+            if i in self.children:
+                included.append(i)
+        return included
+    def parents_in(self,param):
+        included = []
+        for i in param:
+            if i in self.parents:
+                included.append(i)
+        return included
 def is_relator(cls):
     class_type = type(cls)
     if class_type is Relator:
@@ -41,18 +61,32 @@ def type_is_relator(class_type):
             return True
 
 #testing
-class Atom(Relator):
-    def boom():
-        print("explosion")
-class Metal(Atom):
-    def drop():
-        print("sound.play")
-sodium = Metal("sodium")
-oxygen = Atom("oxygen")
+class Person(Relator):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+        self.calculate_followers()
+    
+    def calculate_followers(self):
+        self.followers = len(self.relations)
+class Group(Relator):
+    def __init__(self, name):
+        super().__init__(name)
+        self.calculate_members 
+    def calculate_members(self):
+        self.members = len(self.children)
+user = Person("Tim",17)
+friend = Person("Maddie",16)
+groupmate = Person("Jake",23)
+coding_club = Group("Coding Club")
+user.print_parents()
+user.print_relations()
+coding_club+user
+coding_club+groupmate
+user*friend
+user.print_parents()
+user.print_relations()
+coding_club.print_children()
+print(user.relations_in(user.parents))
+user.parents[0].print_children
 
-sodium*oxygen
-oxygen+sodium
-sodium.print_relations()
-oxygen.print_children()
-sodium.relations[0].name="cheese"
-print(oxygen.name)
