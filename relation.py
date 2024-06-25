db3db88a0a63ce779317ca746c88d33bb1639363
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-06-25 21:09:05 +0800

    print relations or children

--- relation.py
+++ relation.py
@@ -1,11 +1,24 @@
 class Relator:
-    def __init__(self, relations=None, children=None):
+    def __init__(self, name, relations=None, children=None):
+        self.name = name
         self.relations = relations if relations is not None else []
         self.children = children if children is not None else []
 
     def __mul__(self, other):
         self.relations.append(other)
         other.relations.append(self)
+    def __add__(self, other):
+        self.children.append(other)
+    def __str__(self):
+        return self.name
+    def print_relations(self):
+        print(self.name+"'s Relations:")
+        for i in self.relations:
+            print(i.name)
+    def print_children(self):
+        print(self.name+"'s Children:")
+        for i in self.children:
+            print(i.name)
 def is_relator(cls):
     class_type = type(cls)
     if class_type is Relator:
@@ -29,11 +42,17 @@ def type_is_relator(class_type):
 
 #testing
 class Atom(Relator):
-    def __init__(self, name):
-        self.name = name
+    def boom():
+        print("explosion")
 class Metal(Atom):
-    def __init__(self, electrons):
-        self.electrons = electrons
-test = Metal(electrons=0)
-print(is_relator(test))
-print(type_is_relator(type(test)))
+    def drop():
+        print("sound.play")
+sodium = Metal("sodium")
+oxygen = Atom("oxygen")
+
+sodium*oxygen
+oxygen+sodium
+sodium.print_relations()
+oxygen.print_children()
+sodium.relations[0].name="cheese"
+print(oxygen.name)
