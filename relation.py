02073a388c12aa2189148fcba3c3dff4d441c498
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-06-26 13:43:21 +0800

    fixed it

--- relation.py
+++ relation.py
@@ -4,41 +4,37 @@ class Relator:
         self.relations = relations if relations is not None else []
         self.children = children if children is not None else []
         self.parents = parents if parents is not None else []
+    
     def __mul__(self, other):
-        self.relations.append(other)
-        other.relations.append(self)
+        if other not in self.relations:
+            self.relations.append(other)
+        if self not in other.relations:
+            other.relations.append(self)
+    
     def __add__(self, other):
-        self.children.append(other)
-        other.parents.append(self)
+        if other not in self.children:
+            self.children.append(other)
+        if self not in other.parents:
+            other.parents.append(self)
+    
     def __str__(self):
         return self.name
+    
     def print_relations(self):
+        print(f"{self.name}'s Relations:")
         for i in self.relations:
             print(i)
+    
     def print_children(self):
+        print(f"{self.name}'s Children:")
         for i in self.children:
             print(i)
+    
     def print_parents(self):
+        print(f"{self.name}'s Parents:")
         for i in self.parents:
             print(i)
-    def relations_in(self,param):
-        included = []
-        for i in param:
-            if i in self.relations:
-                included.append(i)
-        return included
-    def children_in(self,param):
-        included = []
-        for i in param:
-            if i in self.children:
-                included.append(i)
-        return included
-    def parents_in(self,param):
-        included = []
-        for i in param:
-            if i in self.parents:
-                included.append(i)
-        return included
+
 def is_relator(cls):
     class_type = type(cls)
     if class_type is Relator:
@@ -49,18 +45,17 @@ def is_relator(cls):
         if type_is_relator(base):
             return True
     return False
+
 def type_is_relator(class_type):
     if class_type is Relator:
         return True
-    
     if Relator in class_type.__bases__:
         return True
-    
     for base in class_type.__bases__:
         if type_is_relator(base):
             return True
 
-#testing
+# Testing
 class Person(Relator):
     def __init__(self, name, age):
         super().__init__(name)
@@ -68,25 +63,50 @@ class Person(Relator):
         self.calculate_followers()
     
     def calculate_followers(self):
-        self.followers = len(self.relations)
+        self.followers = len(self.children)
+    
+    def calculate_friends(self):
+        for child in self.children:
+            if self in child.children:
+                self * child
+                child * self
+    
+    def __add__(self, other):
+        re = super().__add__(other)
+        self.calculate_followers()
+        other.calculate_followers()
+        self.calculate_friends()
+        other.calculate_friends()
+        return re
+    
+    def __mul__(self, other):
+        re = super().__mul__(other)
+        return re
+
 class Group(Relator):
     def __init__(self, name):
         super().__init__(name)
-        self.calculate_members 
+        self.calculate_members()
+    
     def calculate_members(self):
         self.members = len(self.children)
-user = Person("Tim",17)
-friend = Person("Maddie",16)
-groupmate = Person("Jake",23)
+    
+    def __add__(self, other):
+        re = super().__add__(other)
+        self.calculate_members()
+        return re
+
+user = Person("Tim", 17)
+friend = Person("Maddie", 16)
+groupmate = Person("Jake", 23)
 coding_club = Group("Coding Club")
-user.print_parents()
+
+user+friend
+user.print_children()
+friend+user
 user.print_relations()
+friend.print_relations()
 coding_club+user
 coding_club+groupmate
-user*friend
-user.print_parents()
-user.print_relations()
 coding_club.print_children()
-print(user.relations_in(user.parents))
-user.parents[0].print_children
-
+print(coding_club.members)
